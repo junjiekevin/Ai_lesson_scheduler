509f5f464de3d9e2057c79735e67dd3ad922ab3e
junjiekevin <leejunjiekevin@Gmail.com>
2024-10-24 12:31:57 -0400

    Updated time range on table to exclude hrs after 6pm

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ function displayScheduleToTeacher(schedule) {
     const timeSlots = [
         "9:00am", "10:00am", "11:00am", "12:00pm", 
         "1:00pm", "2:00pm", "3:00pm", "4:00pm", 
-        "5:00pm", "6:00pm", "7:00pm", "8:00pm", "9:00pm"
+        "5:00pm", "6:00pm"
     ];
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
